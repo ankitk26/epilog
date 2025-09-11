d61775f543000e3fc2cbf4c2a9ac3e3a2f24a6f7
ankitk26 <myselfankit51@gmail.com>
2025-09-12 01:53:51 +0530

    collapse section

--- src/components/media-section-by-status.tsx
+++ src/components/media-section-by-status.tsx
@@ -2,11 +2,14 @@ import type { api } from "@convex/_generated/api";
 import type { Id } from "@convex/_generated/dataModel";
 import { useStore } from "@tanstack/react-store";
 import type { FunctionReturnType } from "convex/server";
-import { BookIcon, ClapperboardIcon } from "lucide-react";
+import { BookIcon, ChevronDown, ClapperboardIcon } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 import { filterStore } from "@/store/filter-store";
 import ListCard from "./list-card";
 import MediaCard from "./media-card";
 import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 
 type Props = {
   logs: FunctionReturnType<typeof api.mediaLogs.all>;
@@ -22,12 +25,26 @@ type Props = {
 export default function MediaSectionByStatus(props: Props) {
   const view = useStore(filterStore, (state) => state.view);
   const mediaType = useStore(filterStore, (state) => state.type);
+  const [collapsed, setCollapsed] = useState(false);
 
   return (
     <div className="space-y-3" key={props.section.status}>
       {/* Section title */}
       <div className="space-y-1">
         <div className="flex items-center gap-2">
+          <Button
+            className="size-7"
+            onClick={() => setCollapsed((prevState) => !prevState)}
+            size="icon"
+            variant="ghost"
+          >
+            <ChevronDown
+              className={cn(
+                "size-3 transition-transform",
+                collapsed ? "-rotate-90" : "rotate-0"
+              )}
+            />
+          </Button>
           <h2 className="font-medium text-lg">{props.section.title}</h2>
           <Badge className="bg-muted text-muted-foreground" variant="secondary">
             {props.logs.length}
@@ -36,7 +53,7 @@ export default function MediaSectionByStatus(props: Props) {
       </div>
 
       {/* Grid cards for media */}
-      {props.logs.length !== 0 && (
+      {!collapsed && props.logs.length !== 0 && (
         <div
           className={
             view === "list"
