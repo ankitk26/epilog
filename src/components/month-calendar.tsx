5cca3fbfefc4e8268226cd0cc149230af3b331f6
ankitk26 <myselfankit51@gmail.com>
2026-04-27 03:25:47 +0530

    change months

--- src/components/month-calendar.tsx
+++ src/components/month-calendar.tsx
@@ -1,4 +1,7 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { useState } from "react";
 import CalendarDay from "./calendar-day";
+import { Button } from "./ui/button";
 
 const months = [
 	"Jan",
@@ -19,39 +22,86 @@ const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export default function MonthCalendar() {
 	const today = new Date();
-
 	const currentMonth = today.getMonth();
 	const currentYear = today.getFullYear();
 
+	const [selectedMonth, setSelectedMonth] = useState(currentMonth);
+	const [selectedYear, setSelectedYear] = useState(currentYear);
+
 	const totalDaysInPreviousMonth = new Date(
-		currentYear,
-		currentMonth,
+		selectedYear,
+		selectedMonth,
 		0,
 	).getDate();
 
 	const totalDaysInCurrentMonth = new Date(
-		currentYear,
-		currentMonth + 1,
+		selectedYear,
+		selectedMonth + 1,
 		0,
 	).getDate();
 
 	const currentMonthFirstDayWeekDay = new Date(
-		currentYear,
-		currentMonth,
+		selectedYear,
+		selectedMonth,
 		1,
 	).getDay();
 
 	const currentMonthLastDayWeekDay = new Date(
-		currentYear,
-		currentMonth + 1,
+		selectedYear,
+		selectedMonth + 1,
 		0,
 	).getDay();
 
+	const goToPreviousMonth = () => {
+		if (selectedMonth === 0) {
+			setSelectedMonth(11);
+			setSelectedYear((prev) => prev - 1);
+		} else {
+			setSelectedMonth((prev) => prev - 1);
+		}
+	};
+
+	const goToNextMonth = () => {
+		if (selectedMonth === 11) {
+			setSelectedMonth(0);
+			setSelectedYear((prev) => prev + 1);
+		} else {
+			setSelectedMonth((prev) => prev + 1);
+		}
+	};
+
+	const goToCurrentMonth = () => {
+		setSelectedMonth(new Date().getMonth());
+		setSelectedYear(new Date().getFullYear());
+	};
+
 	return (
 		<div className="col-span-12 h-full">
-			<h1 className="p-2">
-				{months[currentMonth]} {currentYear}
-			</h1>
+			<div className="flex items-center justify-start">
+				<div className="flex items-center gap-2">
+					<Button variant="outline" onClick={goToCurrentMonth}>
+						Current month
+					</Button>
+					<Button
+						size="icon"
+						variant="outline"
+						onClick={goToPreviousMonth}
+					>
+						<ChevronLeftIcon />
+					</Button>
+					<Button
+						size="icon"
+						variant="outline"
+						onClick={goToNextMonth}
+					>
+						<ChevronRightIcon />
+					</Button>
+				</div>
+
+				<h1 className="p-2">
+					{months[selectedMonth]} {selectedYear}
+				</h1>
+			</div>
 
 			<div className="h-full p-2">
 				<div className="grid grid-cols-7">
