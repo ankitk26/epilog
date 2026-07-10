e0ad31a99dce09fc3276360032746823479af7e5
ankitk26 <myselfankit51@gmail.com>
2026-07-10 11:52:11 +0530

    revert vite+

--- vite.config.ts
+++ vite.config.ts
@@ -2,60 +2,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
-import { lazyPlugins, defineConfig } from "vite-plus";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	fmt: {
-		ignorePatterns: [
-			"pnpm-lock.yaml",
-			"convex/_generated/*",
-			"src/routeTree.gen.ts",
-		],
-		useTabs: true,
-		tabWidth: 4,
-		printWidth: 80,
-		sortImports: {
-			newlinesBetween: false,
-		},
-		sortTailwindcss: {
-			stylesheet: "./src/styles/app.css",
-			functions: ["clsx", "cn"],
-		},
-	},
-	lint: {
-		plugins: ["typescript", "unicorn", "oxc"],
-		categories: {
-			correctness: "error",
-		},
-		rules: {
-			"vite-plus/prefer-vite-plus-imports": "error",
-		},
-		env: {
-			builtin: true,
-		},
-		options: {
-			typeAware: true,
-			typeCheck: true,
-		},
-		jsPlugins: [
-			{
-				name: "vite-plus",
-				specifier: "vite-plus/oxlint-plugin",
-			},
-		],
-	},
 	server: {
 		port: 3000,
 	},
 	resolve: {
 		tsconfigPaths: true,
 	},
-	plugins: lazyPlugins(() => [
-		tailwindcss(),
-		tanstackStart(),
-		nitro(),
-		viteReact(),
-	]),
+	plugins: [tailwindcss(), tanstackStart(), nitro(), viteReact()],
 	ssr: {
 		noExternal: ["@convex-dev/better-auth"],
 	},
